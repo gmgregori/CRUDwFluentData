10f33bca82cccd923e2b2b6a4d90b30b2f857cd8
Gianmaria Gregori <gm_gregori@virgilio.it>
2015-08-30 20:26:49 +0200

    Starting point

---
+++ CRUDwFluentData/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CRUDwFluentData.MvcApplication" Language="C#" %>
---
+++ CRUDwFluentData/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRUDwFluentData.Startup))]
+namespace CRUDwFluentData
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
