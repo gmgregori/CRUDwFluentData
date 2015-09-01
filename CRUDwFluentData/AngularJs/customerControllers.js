54a8f84029af40d9481d76b65e2b036954573fb8
Gianmaria Gregori <gm_gregori@virgilio.it>
2015-09-01 17:46:53 +0200

    Step 3

---
+++ CRUDwFluentData/AngularJs/customerControllers.js
@@ -0,0 +1,12 @@
+﻿angular.module("ChinookApp")
+
+    .controller("CustomersListCtrl", function ($scope, $http) {
+        $http.get('api/Customer').
+        success(function (data, status, headers, config) {
+            $scope.customers = data;
+
+        }).
+        error(function (data, status, headers, config) {
+            console.log('CustomerListCtrl error');
+        });
+    });
